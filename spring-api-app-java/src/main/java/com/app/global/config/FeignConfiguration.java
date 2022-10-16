9555dc362f29b9b7e1ef137317e124d4eed765e6
김택윤 <gw9122@naver.com>
2022-10-16 15:04:29 +0900

    spring cloud open feign을 이용하여 Kakao token 발급 완료

--- spring-api-app-java/src/main/java/com/app/global/config/FeignConfiguration.java
+++ spring-api-app-java/src/main/java/com/app/global/config/FeignConfiguration.java
@@ -5,6 +5,7 @@ import feign.Logger;
 import feign.Retryer;
 import feign.codec.ErrorDecoder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @EnableFeignClients(basePackages = "com.app") // todo 패키지명 수정
-@Import(FeignConfiguration.class)
+@Import(FeignClientsConfiguration.class)
 public class FeignConfiguration {
     @Bean
     Logger.Level feignLoggerLevel() {
---
+++ spring-api-app-java/src/main/java/com/app/web/kakaotoken/KakaoTokenController.java
@@ -0,0 +1,42 @@
+package com.app.web.kakaotoken;
+
+import com.app.global.config.web.dto.KakaoTokenDto;
+import com.app.web.kakaotoken.client.KakaoTokenClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class KakaoTokenController {
+
+    private final KakaoTokenClient kakaoTokenClient;
+
+    @Value("${kakao.client.id}")
+    private String clientId;
+
+    @Value("${kakao.client.secret}")
+    private String clientSecret;
+
+
+    @GetMapping("/login")
+    public String login() {
+        return "loginForm";
+    }
+
+    @GetMapping("/oauth/kakao/callback")
+    public @ResponseBody String loginCallBack(String code) {
+        String contentType = "application/x-www-form-urlencoded;charset=utf-8";
+        KakaoTokenDto.Request kakaoTokenRequestDto = KakaoTokenDto.Request.builder()
+                .client_id(clientId)
+                .client_secret(clientSecret)
+                .grant_type("authorization_code")
+                .code(code)
+                .redirect_uri("http://localhost:8080/oauth/kakao/callback")
+                .build();
+        KakaoTokenDto.Response kakaoToken = kakaoTokenClient.requestKakaoToken(contentType, kakaoTokenRequestDto);
+        return "kakao tokne :" + kakaoToken;
+    }
+}
